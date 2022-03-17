4f3d6af2873792ec9f79fbf7d89f0d75e053b135
Anton Goncharik <goncharik.a.k@gmail.com>
2022-03-17 16:15:27 +0200

    added structure folders + updated config/default.json

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const express = require('express');
 
 const app = express();
@@ -13,7 +14,7 @@ app.post( '/', (req, res) => {
     res.json({message: "You can post to this endpoint..."})
 })
 
-const port = 5000;
-app.listen(port, () => {
-    console.log(`!!!!!!!!!!!!!!!Server running on port ${port}!!!!!!!!!!!!!!!!!!!!!!!!!`)
+const PORT = config.get('PORT') || 5000;
+app.listen(PORT, () => {
+    console.log(`!!!!!!!!!!!!!!!Server running on port ${PORT}!!!!!!!!!!!!!!!!!!!!!!!!!`)
 })
