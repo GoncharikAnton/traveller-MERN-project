0bbad5fea9b64a77f5381ff52ffed2764937712f
Anton Goncharik <goncharik.a.k@gmail.com>
2022-03-19 13:30:31 +0200

    Client: 1) Setted up materialize(added script to index.html/added link to materialize icons); 2) Navbar created(+optimized for mobiles)

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,15 @@
 import './App.css';
 import 'materialize-css';
+// import Navbar from "./Components/Navbar/Navbar";
 import {Navbar} from "./Components/Navbar/Navbar";
 import {useCallback, useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
-    const [tours, setTours] = useState([])
-
-    const data = useCallback(async () => {
-        try {
-            const response = await fetch('/api/v1/tours', {method: 'GET'})
-            const data = await response.json()
-            console.log(...Object.values(data.data.tours))
-            await setTours([...Object.values(data.data.tours)])
-
-        }catch (e){
-            console.log(e.message)}
-    }, [tours])
-
-    useEffect(() => {
-        data()
-        return () => {}
-    }, [])
-
-
-
 
     return (
-        <div className="App container">
-            <Navbar/>
-            <h1>Hello Traveller!</h1>
-            {/*<button onClick={data}>DATA</button>*/}
-            {tours && tours.map((tour, index) => {
-                console.log(tour)
-                return <li key={index}>
-                    <h1>{tour.name}</h1>
-                    <h5>{tour.description}</h5>
-                    <h4>{tour.price}</h4>
-                </li>
-            })}
+        <div className="App">
+        <Navbar/>
         </div>
     );
 }
--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import 'materialize-css'
+import M from  'materialize-css/dist/js/materialize.min.js';
 
 export const Navbar = () => {
 
+    useEffect(() => {
+    var elem = document.querySelector(".sidenav");
+    var instance = M.Sidenav.init(elem, {
+        edge: "right",
+        inDuration: 250
+    });
+        }, [])
 
-    return (
-        <nav>
-            <div className="nav-wrapper cyan lighten-1">
-                <a href="#" className="brand-logo">Logo</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
-                </ul>
-            </div>
-        </nav>
-    )
+        return (
+                <nav>
+                    <div className="nav-wrapper cyan lighten-1">
+                        <a href="#!" className="brand-logo">Logo</a>
+                        <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i
+                            className="material-icons">menu</i></a>
+                        <ul className="right hide-on-med-and-down">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Blogs</a></li>
+                            <li><a href="#">Tours</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Contacts</a></li>
+                            <li><a href="#">Login/Register</a></li>
+                        </ul>
+                    </div>
+                    <ul className="sidenav" id="mobile-demo">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">Tours</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contacts</a></li>
+                        <li><a href="#">Login/Register</a></li>
+                    </ul>
+                </nav>
+        )
 }
 
 
