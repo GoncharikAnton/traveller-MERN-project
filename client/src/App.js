6765fc41baf74859dd7a8244fa02e923e16b4bcb
Anton Goncharik <goncharik.a.k@gmail.com>
2022-03-19 11:18:35 +0200

    1) New project structure. 2) Working with data from the server(App.js for now)

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
 import 'materialize-css';
-import {Navbar} from "./components/Navbar/Navbar";
-import {useCallback, useState} from "react";
+import {Navbar} from "./Components/Navbar/Navbar";
+import {useCallback, useEffect, useState} from "react";
 import axios from "axios";
-import {type} from "@testing-library/user-event/dist/type";
 
 function App() {
-    const [tours, setTours] = useState(null)
-// TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    const data = async () => {
+    const [tours, setTours] = useState([])
+
+    const data = useCallback(async () => {
         try {
             const response = await fetch('/api/v1/tours', {method: 'GET'})
-            // console.log(response)
             const data = await response.json()
-            console.log(data.data)
-            setTours({...data.data});
-            console.log(tours.tours)
+            console.log(...Object.values(data.data.tours))
+            await setTours([...Object.values(data.data.tours)])
+
         }catch (e){
-            console.log(e)}
-    }
+            console.log(e.message)}
+    }, [tours])
+
+    useEffect(() => {
+        data()
+        return () => {}
+    }, [])
 
 
 
@@ -27,10 +30,15 @@ function App() {
         <div className="App container">
             <Navbar/>
             <h1>Hello Traveller!</h1>
-            <button onClick={data}>DATA</button>
-            {/*{tours && tours.tours.map((tour, index) => {*/}
-            {/*    return <li key={index}>{tour}</li>*/}
-            {/*})}*/}
+            {/*<button onClick={data}>DATA</button>*/}
+            {tours && tours.map((tour, index) => {
+                console.log(tour)
+                return <li key={index}>
+                    <h1>{tour.name}</h1>
+                    <h5>{tour.description}</h5>
+                    <h4>{tour.price}</h4>
+                </li>
+            })}
         </div>
     );
 }
