ecb6b302971d5fee5594021d06e4d9c0c9a7a5b6
Anton Goncharik <goncharik.a.k@gmail.com>
2022-04-18 13:24:19 +0300

    Registartion form

--- client/src/layouts/main/LoginRegister/LoginRegister.js
+++ client/src/layouts/main/LoginRegister/LoginRegister.js
@@ -1,20 +1,23 @@
 import React, {useContext, useEffect, useState} from 'react'
 import {AuthContext} from "../../../context/AuthContext";
-import {Link, Navigate} from "react-router-dom";
+import axios from "axios";
+import {useNavigate} from "react-router";
 
 import styles from './LoginRegister.module.css'
-import Header from "../../header/Header";
-import axios from "axios";
 
 const LoginRegister = () => {
 
+    const [registration, setRegistration] = useState(false)
     const auth = useContext(AuthContext)
     const [form, setForm] = useState({
-        email: '', password: ''
+        name: '', email: '', password: '', passwordConfirm: ''
     })
+    console.log(form)
     const changeHandler = event => {
         setForm({...form, [event.target.name]: event.target.value})
     }
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         window.M.updateTextFields()
@@ -28,7 +31,21 @@ const LoginRegister = () => {
         } catch (e) {
         }
     }
-    return(
+
+    const registerHandler_1 = () => {
+        setRegistration(true)
+    }
+    const registerHandler = async () => {
+        setRegistration(false)
+        try {
+            const response = await axios.post('/api/v1/users/signup', {...form})
+            auth.login(response.data.token, response.data.data.user._id, response.data.data.user.role)
+        } catch (e) {
+        }
+    }
+
+
+    return (
         <>
             <div className={`row ${styles}`}>
                 <div className="container">
@@ -39,58 +56,131 @@ const LoginRegister = () => {
                             <span className="card-title black-text bold-text">Authorization</span>
                         </div>
 
-                        <div>
-                            <div className="input-field">
-                                <input
-                                    id="email"
-                                    type="text"
-                                    name={'email'}
-                                    className={`${styles['yellow-input']}`}
-                                    value={form.email}
-                                    onChange={changeHandler}
+                        {!registration ? <>
+                                <div>
+                                    <div className="input-field">
+                                        <input
+                                            id="email"
+                                            type="text"
+                                            name={'email'}
+                                            className={`${styles['yellow-input']}`}
+                                            value={form.email}
+                                            onChange={changeHandler}
 
-                                />
-                                <label htmlFor="email">Email</label>
-                            </div>
+                                        />
+                                        <label htmlFor="email">Email</label>
+                                    </div>
 
-                            <div className="input-field">
-                                <input
-                                    id="password"
-                                    type="password"
-                                    name={'password'}
-                                    className={`${styles['yellow-input']}`}
-                                    value={form.password}
-                                    onChange={changeHandler}
-                                />
-                                <label htmlFor="password">Password</label>
-                            </div>
+                                    <div className="input-field">
+                                        <input
+                                            id="password"
+                                            type="password"
+                                            name={'password'}
+                                            className={`${styles['yellow-input']}`}
+                                            value={form.password}
+                                            onChange={changeHandler}
+                                        />
+                                        <label htmlFor="password">Password</label>
+                                    </div>
 
-                        </div>
+                                </div>
 
-                        <div className="card-action">
-                            <button
-                                className={'btn yellow darken-4'}
-                                style=
-                                    {{
-                                        marginRight: 10
-                                    }}
-                                // disabled={loading}
-                                onClick={loginHandler}
-                            >
-                                Login
-                                {auth.isAuthenticated ? <Navigate to={'/'}></Navigate>
-                                : null
-                                }
+                                <div className="card-action">
+                                    <button
+                                        className={'btn yellow darken-4'}
+                                        style=
+                                            {{
+                                                marginRight: 10
+                                            }}
+                                        // disabled={loading}
+                                        onClick={loginHandler}
+                                    >
+                                        Login
+                                        {auth.isAuthenticated ? navigate('/')
+                                            : null
+                                        }
 
-                            </button>
-                            <button
-                                className={'btn grey lighten-1 black-text'}
-                                // onClick={registerHandler}
-                                // disabled={loading}
-                            >Registration
-                            </button>
+                                    </button>
 
-                        </div>
+                                    <button
+                                        className={'btn grey lighten-1 black-text'}
+                                        onClick={registerHandler_1}
+                                        // disabled={loading}
+                                    >Registration
+                                    </button>
+
+                                </div>
+                            </> :
+                            <>
+                                <div>
+                                    <div className="input-field">
+                                        <input
+                                            id="name"
+                                            type="text"
+                                            name={'name'}
+                                            className={`${styles['yellow-input']}`}
+                                            value={form.name}
+                                            onChange={changeHandler}
+                                        />
+                                        <label htmlFor="Name">Name</label>
+                                    </div>
+
+                                    <div className="input-field">
+                                        <input
+                                            id="email"
+                                            type="text"
+                                            name={'email'}
+                                            className={`${styles['yellow-input']}`}
+                                            value={form.email}
+                                            onChange={changeHandler}
+                                        />
+                                        <label htmlFor="email">Email</label>
+                                    </div>
+
+                                    <div className="input-field">
+                                        <input
+                                            id="password"
+                                            type="password"
+                                            name={'password'}
+                                            className={`${styles['yellow-input']}`}
+                                            value={form.password}
+                                            onChange={changeHandler}
+                                        />
+                                        <label htmlFor="password">Password</label>
+                                    </div>
+                                    <div className="input-field">
+                                        <input
+                                            id="passwordConfirm"
+                                            type="password"
+                                            name={'passwordConfirm'}
+                                            className={`${styles['yellow-input']}`}
+                                            value={form.passwordConfirm}
+                                            onChange={changeHandler}
+                                        />
+                                        <label htmlFor="password_confirm">Password confirm</label>
+                                    </div>
+
+                                </div>
+
+                                <div className="card-action">
+                                    <button
+                                        className={'btn yellow darken-4'}
+                                        style=
+                                            {{
+                                                marginRight: 10
+                                            }}
+                                        // disabled={loading}
+                                        onClick={registerHandler}
+                                    >
+                                        Registration
+                                        {auth.isAuthenticated ? navigate('/')
+                                            : null
+                                        }
+                                    </button>
+
+                                </div>
+                            </>
+                        }
                     </div>
 
                 </div>
--- qwe.js
+++
@@ -1,9 +0,0 @@
-const text = "-[--(++[+)---]-"
-
-function verify(text){
-    console.log(text.search(/\)|]|</g))
-    console.log(text.search(/\(|\[|>/g))
-    return text.substring(text.search(/\)|]|</g) + 1, text.search(/\(|\[|>/g) )
-}
-
-console.log(verify(text))
